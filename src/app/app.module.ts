6a4635fa527656d4a1e9ae0ec5ce50e83da41481
Chris Mannel <cmannel77@gmail.com>
2017-12-30 23:41:10 -0600

    Removed component testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
