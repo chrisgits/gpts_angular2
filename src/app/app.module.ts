e3edfd5ce2d0450f01d1c875a188f605e922a423
Chris Mannel <cmannel77@gmail.com>
2018-01-02 22:56:34 -0600

    video and ng nav implemented
    video or nav not working correctly
    nav ng is close

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavComponent } from './nav/nav.component';
+import { NgbdCollapseBasic } from './nav/nav.component';
 import { ContentBodyComponent } from './content-body/content-body.component';
 import { ContentsuasComponent } from './contentsuas/contentsuas.component';
 import { JumboComponent } from './jumbo/jumbo.component';
 import { ContentschedComponent } from './contentsched/contentsched.component';
 import { ContentAboutComponent } from './content-about/content-about.component';
 
-
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    NavComponent,
-    ContentBodyComponent,
-    ContentsuasComponent,
-    JumboComponent,
-    ContentschedComponent,
-    ContentAboutComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
+    NgbModule,
     RouterModule.forRoot ([
       {
         path: '',
@@ -48,6 +39,18 @@ import { ContentAboutComponent } from './content-about/content-about.component';
         component: ContentsuasComponent
       },
     ])
+  ],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ContentBodyComponent,
+    ContentsuasComponent,
+    JumboComponent,
+    ContentschedComponent,
+    ContentAboutComponent
+
   ],
   providers: [],
   bootstrap: [AppComponent]
